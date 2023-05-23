221ca84cc5360ca2034e53fde17ed1645e50ce12
Lilian Daminelli Aguirre <lilidaminelli@gmail.com>
2023-05-23 15:52:36 +0200

    add card images intophoto gallery

--- src/components/Tags/index.jsx
+++ src/components/Tags/index.jsx
@@ -6,10 +6,10 @@ function Tags() {
       <div className="tags-container">
         <p className="tags-info">Search by tag:</p>
         <ul className="tags-list">
-          <li className="tags-item"> Stars</li>
-          <li className="tags-item"> Galaxies</li>
-          <li className="tags-item"> Moon</li>
-          <li className="tags-item"> Planets</li>
+          <li className="tags-item">Stars</li>
+          <li className="tags-item">Galaxies</li>
+          <li className="tags-item">Moon</li>
+          <li className="tags-item">Planets</li>
         </ul>
       </div>
     </>
